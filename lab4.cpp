cd442e87a421eb584acf023993f99c43c5e2fea8
aniraj kumar <45869786+aniraj001@users.noreply.github.com>
2019-08-22 19:25:12 +0530

    Update lab4.cpp

--- lab4.cpp
+++ lab4.cpp
@@ -1,477 +1,265 @@
-#include <bits/stdc++.h>
-#include <math.h>
- 
-#define io ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define PB push_back
-#define MP make_pair
-#define REP(i,a,b) for (ll i = a; i < b; i++)
-#define enl "\n"
-#define INF std::numeric_limits<ll>::max()
-#define F first
-#define S second
- 
+
+#include<bits/stdc++.h>
 using namespace std;
- 
-typedef long long int ll;
-typedef unsigned long long int ull;
-typedef long double ldb;
-typedef vector<long long> vll;
-typedef pair<long long,long long> pll;
- 
-ll greatestSubarraySum(ll array[], ll n);
-ll powerrr(ll a, ll b);
-void swapp(ll *a, ll*b);
-ll gcd(ll a, ll b);
-ll lcm(ll a, ll b);
-ll min(ll a, ll b);
-ll max(ll a, ll b);
-ll search(ll arr[], ll n, ll k);
-string findSum(string str1, string str2);
- 
- 
- 
- 
- 
- 
-string findSum(string str1, string str2) 
-{ 
-    // Before proceeding further, make sure length 
-    // of str2 is larger. 
-    if (str1.length() > str2.length()) {
-        string s = str1;
-        str1=str2;
-        str2=s;
-    }
-  
-    // Take an empty string for storing result 
-    string str = ""; 
-  
-    // Calculate length of both string 
-    int n1 = str1.length(), n2 = str2.length(); 
-  
-    // Reverse both of strings 
-    reverse(str1.begin(), str1.end()); 
-    reverse(str2.begin(), str2.end()); 
-  
-    int carry = 0; 
-    for (int i=0; i<n1; i++) 
-    { 
-        // Do school mathematics, compute sum of 
-        // current digits and carry 
-        int sum = ((str1[i]-'0')+(str2[i]-'0')+carry); 
-        str.push_back(sum%10 + '0'); 
-  
-        // Calculate carry for next step 
-        carry = sum/10; 
-    } 
-  
-    // Add remaining digits of larger number 
-    for (int i=n1; i<n2; i++) 
-    { 
-        int sum = ((str2[i]-'0')+carry); 
-        str.push_back(sum%10 + '0'); 
-        carry = sum/10; 
-    } 
-  
-    // Add remaining carry 
-    if (carry) 
-        str.push_back(carry+'0'); 
-  
-    // reverse resultant string 
-    reverse(str.begin(), str.end()); 
-  
-    return str; 
-} 
- 
- 
- 
-ll search(ll arr[], ll n, ll k){
-    
-    ll u = k;
-    ll l = 0;
- 
-    
-    while(u>=l){
-        
-        if(arr[(u+l)/2] == n){
-            
-            return (u+l)/2;
-            
-        } else if(arr[(u+l)/2] > n){
-            
-            u = (u+l)/2 - 1;
-            
-        } else {
-            
-            l = (u+l)/2 + 1;
-            
-        }
-        
-        
-    }
-    
-    return -1;
-    
-    
-}
- 
-ll greatestSubarraySum(ll array[], ll n){
- 
-    ll best = 0, sum = 0;
-    for (ll k = 0; k < n; k++) {
-        sum = max(array[k],sum+array[k]);
-        best = max(best,sum);
-    }
- 
-    return best;
-}
- 
-ll powerrr(ll a, ll b){
- 
-    ll ans = 1;
- 
-    for(ll i = 0; i < b; i++){
- 
- 
-        ans*= a;
- 
- 
-    }
- 
-    return ans;
- 
- 
-}
- 
-void swapp(ll *a, ll*b){
- 
-    ll temp = *a;
-    *a = *b;
-    *b = temp;
- 
- 
-}
- 
- 
- 
-ll gcd(ll a, ll b)
+#define ll long long
+#define REP(i,a,b) for(ll i=a;i<b;i++)
+#define REPI(i,a,b) for(ll i=b-1;i>=a;i--)
+#define PB push_back
+
+ll prec(char c)
 {
-    if (a == 0)
-        return b;
-    
-    return gcd(b%a, a);
-}
- 
-ll lcm(ll a, ll b)  
-{  
-    return (a*b)/gcd(a, b);  
-} 
- 
-ll min(ll a, ll b){
-    if(a < b)return a;  
-    return b;
-}
- 
-ll max(ll a, ll b){
-    if(a > b)return a;
-    return b;
+    switch(c)
+    {
+        case '+':
+            return 0;
+        case '-':
+            return 0;
+        case '*':
+            return 1;
+        case '/':
+            return 1;
+        case '^':
+            return 2;
+        default:
+            return -1;
+    }
+    return -1;
 }
 
-set<char> signs;
-
-ll flagl;
-
-stack<char> brac;
-
-struct node{
-
-    char a;
-    ll b;
-    struct node *left;
-    struct node *right;
-
-
+struct llchar
+{
+    bool f;
+    ll x;
+    char c;
 };
 
-struct node *createNode(){
 
+struct node
+{
+    char c;
+    ll dat;
+    struct node *parent,*left,*right;
+};
+
+struct node *createNode()
+{
     struct node *temp=(struct node *)malloc(sizeof(struct node));
-    temp->a='n';
-    temp->b=-1928;
-
     return temp;
-
 }
 
-ll evaluate(struct node *start){
-
-    if(start->a != 'n'){
-
-        if(start->a == '^'){
-
-            return powerrr(evaluate(start->left), evaluate(start->right));
-
-        } else if(start->a == '*'){
-
-            return evaluate(start->left)*evaluate(start->right);
-
-        } else if(start->a == '/'){
-
-
-            return evaluate(start->left)/evaluate(start->right);
-
-
-        } else if(start->a=='+'){
-
-            return evaluate(start->left)+evaluate(start->right);
-
-        } else {
-
-            return evaluate(start->left)-evaluate(start->right);
-
-        }
-
-    } else {
-
-        return start->b;
-
-    }
-
-
-}
-
-void build(string s, struct node *start){
-
-    REP(i,0,s.length()){
-
-        if(signs.find(s[i]) == signs.end()){
-
-            flagl=1;
-            return;
-
-        }
-
-    }
-
-    if(s[0]=='-'){
-
-        start->b=stoll(s);
-
-        return;
-
-
-    }
-
-    for(ll i=s.length()-1; i>=0; i--){
-
-        //cout<<'a'<<endl;
-
-        if(signs.find(s[i]) == signs.end()){
-
-            flagl=1;
-            return;
-
-        }
-
-        if(s[i] == ')')brac.push(s[i]);
-        if(s[i] == '(')brac.pop();
-
-        if(!(brac.empty()))continue;
-
-        if(s[i]=='+' || s[i]=='-'){
-
-            //cout<<i<<"tatd"<<endl;
-
-            start->a=s[i];
-            string b=s.substr(0,i);
-            string c=s.substr(i+1, s.length()-1-i);
-
-            //cout<<"a"<<b<<" "<<c<<endl;
-
-            start->left=createNode();
-            start->right=createNode();
-            build(b, start->left);
-            build(c, start->right);
-
-            return;
-
-        }
-
-
-    }
-
-    for(ll i=s.length()-1; i>=0; i--){
-
-        //cout<<'b'<<endl;
-
-        if(signs.find(s[i]) == signs.end()){
-
-            flagl=1;
-            return;
-
-        }
-
-        if(s[i] == ')')brac.push(s[i]);
-        if(s[i] == '(')brac.pop();
-
-        if(!(brac.empty()))continue;
-
-        if(s[i]=='*' || s[i]=='/'){
-
-            
-
-
-            start->a=s[i];
-            string b=s.substr(0,i);
-            string c=s.substr(i+1, s.length()-1-i);
-
-            //cout<<"b"<<b<<" "<<c<<endl;
-
-
-            start->left=createNode();
-            start->right=createNode();
-            build(b, start->left);
-            build(c, start->right);
-
-            return;
-
-        }
-
-
-    }
-
-    REP(i,0,s.length()){
-
-        //cout<<'c'<<endl;
-
-        if(signs.find(s[i]) == signs.end()){
-
-            flagl=1;
-            return;
-
-        }
-
-        if(s[i] == '(')brac.push(s[i]);
-        if(s[i] == ')')brac.pop();
-
-        if(!(brac.empty()))continue;
-
-        if(s[i]=='^'){
-
-            start->a='^';
-            string b=s.substr(0,i);
-            string c=s.substr(i+1, s.length()-1-i);
-
-            //cout<<"c"<<b<<" "<<c<<endl;
-
-
-            start->left=createNode();
-            start->right=createNode();
-            build(b, start->left);
-            build(c, start->right);
-
-            return;
-
-        }
-
-    }
-
-    REP(i,0,s.length()){
-
-        //cout<<'d'<<endl;
-
-        if(signs.find(s[i]) == signs.end()){
-
-            flagl=1;
-            return;
-
-        }
-
-        if(s[i]=='('){
-
-            stack<char> flag;
-            flag.push('(');
-
-            REP(j, i+1, s.length()){
-
-                if(s[j]=='(')flag.push('(');
-
-                if(s[j]==')'){
-
-                    flag.pop();
-                    
-
-                }
-
-                if(flag.empty()){
-                    string b=s.substr(i+1, j-1-i);
-
-                    //cout<<'d'<<" "<<b<<endl;
-
-                    build(b, start);
-                    return;
-                }
-
+vector<llchar> itop(string s,bool &error)
+{
+    stack<char> st;
+    st.push('0');
+    ll n=s.size();
+    vector<llchar> tt;
+    bool flag=0;
+    REP(i,0,n)
+    {
+        if(s[i]<='9'&&s[i]>='0')
+        {
+            if(flag==1)
+            {
+                tt[tt.size()-1].x*=10;
+                tt[tt.size()-1].x+=(s[i]-'0');
             }
-
-
-
+            else
+            {
+                llchar temp;
+                temp.x=(s[i]-'0');
+                temp.f=0;
+                tt.PB(temp);
+                flag=1;
+            }
+        }
+        else if(s[i] == '(')
+        {
+            st.push(s[i]);
+            flag=0;
+        }
+        else if(s[i] == ')')
+        {
+            while(st.top()!='0'&&st.top()!='(')
+            {
+                llchar temp;
+                temp.f=1;
+                temp.c=st.top();
+                st.pop();
+                tt.PB(temp);
+            }
+            if(st.top()=='(')
+            {
+                st.pop();
+            }
+            else
+            {
+                error=1;
+                return tt;
+            }
+            flag=0;
+        }
+        else if(prec(s[i])>=0)
+        {
+            while(st.top()!='0'&&prec(s[i])<=prec(st.top()))
+            {
+                if(s[i]=='^'&&st.top()=='^') break;
+                llchar temp;
+                temp.c=st.top();
+                temp.f=1;
+                st.pop();
+                tt.PB(temp);
+            }
+            st.push(s[i]);
+            flag=0;
+        }
+        else
+        {
+            error=1;
+            return tt;
         }
 
     }
-
-    start->b=stoll(s);
-
-
-
+    while(st.top()!='0')
+    {
+        llchar temp;
+        temp.c=st.top();
+        temp.f=1;
+        st.pop();
+        tt.PB(temp);
+    }
+    return tt;
 }
 
+ll pow(ll x,ll y)
+{
+    ll t=1;
+    while(y--)
+    {
+        t*=x;
+    }
+    return t;
+}
 
-int main(){
- 
-    io
- 
-    signs.insert('^');
-    signs.insert('+');
-    signs.insert('*');
-    signs.insert('-');
-    signs.insert('/');
-    signs.insert('(');
-    signs.insert(')');
-    signs.insert('0');
-    signs.insert('1');
-    signs.insert('2');
-    signs.insert('3');
-    signs.insert('4');
-    signs.insert('5');
-    signs.insert('6');
-    signs.insert('7');
-    signs.insert('8');
-    signs.insert('9');
+ll eval(struct node* root)
+{
+    if(root->left==NULL)
+    {
+        return root->dat;
+    }
+    else
+    {
+        switch (root->c)
+        {
+            case '+':
+                return eval(root->right)+eval(root->left);
+            case '-':
+                return eval(root->right)-eval(root->left);
+            case '*':
+                return eval(root->right)*eval(root->left);
+            case '/':
+                return eval(root->right)/eval(root->left);
+            case '^':
+                return pow(eval(root->right),eval(root->left));
+        }
+    }
+    return -1;
+}
 
-
-
-    ll n;
-    cin>>n;
-
-    while(n--){
-
-        ll q;
-        cin>>q;
-
-        while(q--){
-            flagl=0;
-
+int main()
+{
+    ll q;
+    cin>>q;
+    while(q--)
+    {
+        ll n;
+        cin>>n;
+        while(n--)
+        {
+            string s0;
+            cin>>s0;
+            ll flag_unary=1;
             string s;
-            cin>>s;
-            struct node *start=createNode();
-
-            build(s, start);
-            ll x=evaluate(start);
-            if(flagl)cout<<"CANT BE EVALUATED"<<endl;
-            else cout<<x<<endl;
+            REP(i,0,s0.size())
+            {
+                  if(flag_unary==1&&s0[i]=='-') s+="0";
+                  else if(s0[i]>='0'&&s0[i]<='9') flag_unary=0;
+                  else flag_unary=1;
+                  s+=s0[i];
+            }
+            struct node *root;
+            struct node *cur;
+            bool error=0;
+            vector<llchar> postfix=itop(s,error);
+            if(error==1)
+            {
+                cout<<"CANT BE EVALUATED\n";
+                continue;
+            }
+            REPI(i,0,postfix.size())
+            {
+                if(postfix[i].f==0&&i==postfix.size()-1)
+                {
+                    struct node *temp=createNode();
+                    temp->dat=postfix[i].x;
+                    temp->left=temp->right=temp->parent=NULL;
+                    cur=root=temp;
+                }
+                else if(postfix[i].f==1&&i==postfix.size()-1)
+                {
+                    struct node *temp=createNode();
+                    temp->c=postfix[i].c;
+                    temp->left=temp->right=temp->parent=NULL;
+                    cur=root=temp;
+                }
+                else if(postfix[i].f==0)
+                {
+                    if(cur->left==NULL)
+                    {
+                        struct node *temp=createNode();
+                        temp->dat=postfix[i].x;
+                        temp->left=temp->right=NULL;
+                        temp->parent=cur;
+                        cur->left=temp;
+                    }
+                    else
+                    {
+                        struct node *temp=createNode();
+                        temp->dat=postfix[i].x;
+                        temp->left=temp->right=NULL;
+                        temp->parent=cur;
+                        cur->right=temp;
+                        while(cur->right!=NULL)
+                        {
+                            if(cur->parent==NULL) break;
+                            cur=cur->parent;
+                        }
+                    }
+                }
+                else
+                {
+                    if(cur->left==NULL)
+                    {
+                        struct node *temp=createNode();
+                        temp->c=postfix[i].c;
+                        temp->left=temp->right=NULL;
+                        temp->parent=cur;
+                        cur->left=temp;
+                        cur=temp;
+                    }
+                    else
+                    {
+                        struct node *temp=createNode();
+                        temp->c=postfix[i].c;
+                        temp->left=temp->right=NULL;
+                        temp->parent=cur;
+                        cur->right=temp;
+                        cur=temp;
+                    }
+                }
+            }
+            cout<<eval(root)<<'\n';
         }
     }
 
-  
+    return 0;
 }
